6d811155993430f7aa3a768a6795359db9f098f9
jonasdevzero <jonasdevzero@gmail.com>
2021-02-13 11:42:54 -0300

    config frontend

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    };
+    html {
+        font-size: 62.5%;
+    };
+    body {
+        color: #000;
+        font-family: 'Roboto', sans-serif;
+    };
+    #root {
+        width: 100vw;
+        height: 100%;
+    };
+`;
