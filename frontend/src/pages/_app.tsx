23fd2c98450161c27d9423aa093a1197e0e10485
jonasdevzero <jonasdevzero@gmail.com>
2021-02-14 18:18:54 -0300

    adding theme

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import GlobalStyle from '../styles/global';
+import { ThemeProvider } from 'styled-components';
+import usePersistedState from '../hooks/usePersistedState';
 
 function MyApp({ Component, pageProps }) {
+    const [theme, setTheme] = usePersistedState<"light" | "dark">("theme", "light")
+
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <GlobalStyle />
             <Component {...pageProps} />
-        </>
+        </ThemeProvider>
     );
 };
 
