35874c59fcde08f8717b276fdcd8d127775a6424
jonasdevzero <jonasdevzero@gmail.com>
2021-02-13 23:52:13 -0300

    finish backend v0.1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,34 @@ import express from 'express';
 import { Server } from 'http';
 import socketio from 'socket.io';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 const server = new Server(app);
-const io = new socketio.Server(server)
+const io = new socketio.Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+})
 
 const PORT = process.env.PORT || 3001;
 
 io.on('connection', (socket: socketio.Socket) => {
-    console.log("New Connetion!");
-    console.log(socket.id);
+    socket.on("join", (room, callback) => {
+        console.log(room);
+        socket.join(room);
+
+        callback();
+    });
+
+    socket.on("sendMessage", ({ message, room }, callback) => {
+        io.to(room).emit("message", message);
+
+        callback();
+    });
 });
 
+app.use(cors());
 app.use(routes);
 
 server.listen(PORT, () => console.log("Server running -> " + PORT));
