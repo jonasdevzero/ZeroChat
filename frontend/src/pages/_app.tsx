0d67e535d2731952c7a37787a4fb5c6bf5593bd7
jonasdevzero <jonasdevzero@gmail.com>
2021-02-14 22:35:06 -0300

    fix

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import GlobalStyle from '../styles/global';
 import { ThemeProvider } from 'styled-components';
 import usePersistedState from '../hooks/usePersistedState';
+import { light, dark } from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
-    const [theme, setTheme] = usePersistedState<"light" | "dark">("theme", "light")
+    const [theme, setTheme] = usePersistedState<"light" | "dark">("theme", "light");
 
     return (
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme === 'light' ? light : dark}>
             <GlobalStyle />
             <Component {...pageProps} />
         </ThemeProvider>
