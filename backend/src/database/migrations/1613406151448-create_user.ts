200675fb3cf9a601ff79a7b0fc5caa592e3df003
jonasdevzero <jonasdevzero@gmail.com>
2021-02-15 14:56:48 -0300

    adding user

---
+++ backend/src/database/migrations/1613406151448-create_user.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createUser1613406151448 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "user",
+            columns: [
+                {
+                    name: "id",
+                    type: "integer",
+                    isGenerated: true,
+                    isPrimary: true,
+                    unsigned: true,
+                    generationStrategy: "increment",
+                },
+                {
+                    name: "name",
+                    type: "varchar",
+                },
+                {
+                    name: "username",
+                    type: "varchar",
+                },
+                {
+                    name: "email",
+                    type: "varchar",
+                },
+                {
+                    name: "email",
+                    type: "varchar",
+                },
+                {
+                    name: "picture",
+                    type: "text",
+                    default: "https://github.com/jonasdevzero/MediaHub/blob/master/images/profilePicture.png?raw=true",
+                },
+                {
+                    name: "resetToken",
+                    type: "varchar",
+                    isNullable: true,
+                },
+                {
+                    name: "expiresToken",
+                    type: "varchar",
+                    isNullable: true,
+                },
+            ],
+        }));
+    };
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("user");
+    };
+
+};
---
+++ backend/src/models/User.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity("user")
+export default class User {
+    @PrimaryGeneratedColumn("increment")
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    picture: string;
+
+    @Column()
+    resetToken: string;
+
+    @Column()
+    expiresToken: string;
+};
