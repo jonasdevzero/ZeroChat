8d66595d541548c258736f4a814b502ed8c2d1b5
jonasdevzero <jonasdevzero@gmail.com>
2021-02-15 13:06:10 -0300

    adding a style theme

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,3 +1,17 @@
-export const light = {}
+export const light = {
+    color: '#000',
+    backgroundColor: '#fff',
+    header: {
+        color: '#fff',
+        backgroundColor: '#191818'
+    },
+}
 
-export const dark = {}
+export const dark = {
+    color: '#fff',
+    backgroundColor: '#191818',
+    header: {
+        color: '#fff',
+        backgroundColor: '#000'
+    },
+}
