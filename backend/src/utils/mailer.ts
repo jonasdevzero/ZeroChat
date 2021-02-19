ead27a08612748d5fa5848f169ab76b9f0936be4
jonasdevzero <jonasdevzero@gmail.com>
2021-02-19 17:02:08 -0300

    fix

--- backend/src/utils/mailer.ts
+++ backend/src/utils/mailer.ts
@@ -4,7 +4,7 @@ export default nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-        user: "7daad4f787a74f",
-        pass: "2e8a1e039f9d9e"
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASS
     }
 });
