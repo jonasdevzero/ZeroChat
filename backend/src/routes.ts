99c8ff6c93b939432f078ff8c1a280cc55be6d33
jonasdevzero <jonasdevzero@gmail.com>
2021-02-20 23:02:30 -0300

    reording routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,12 +7,15 @@ routes.get("/", (req, res) => res.status(200).json("ok"));
 
 routes.get("/user", UserController.index);
 routes.get("/user/:username", UserController.show);
+
 routes.post("/user", UserController.create);
-routes.put("/user/:id", UserController.auth, UserController.update);
-routes.delete("/user/:id", UserController.auth, UserController.delete);
 routes.post("/user/auth", UserController.auth);
 routes.post("/user/login", UserController.login);
 routes.post("/user/forgot_password", UserController.forgotPassword);
+
+routes.delete("/user/:id", UserController.auth, UserController.delete);
+
 routes.put("/user/reset_password", UserController.resetPassword);
+routes.put("/user/:id", UserController.auth, UserController.update);
 
 export default routes;
