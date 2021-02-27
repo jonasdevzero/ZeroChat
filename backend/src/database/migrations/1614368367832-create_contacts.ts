24b24a9fe12ff6bb0ce702959d00eb03ece546dc
jonasdevzero <jonasdevzero@gmail.com>
2021-02-26 22:57:24 -0300

    adding contact migration

---
+++ backend/src/database/migrations/1614368367832-create_contacts.ts
@@ -0,0 +1,57 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createContacts1614368367832 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "contacts",
+            columns: [
+                {
+                    name: "id",
+                    type: "varchar",
+                    isPrimary: true,
+                    unsigned: true,
+                    isUnique: true,
+                },
+                {
+                    name: "user_id",
+                    type: "varchar",
+                },
+                {
+                    name: "contact_id",
+                    type: "varchar",
+                },
+                {
+                    name: "contact_username",
+                    type: "varchar",
+                },
+                {
+                    name: "contact_image",
+                    type: "text",
+                    isNullable: true,
+                }, 
+                {
+                    name: "last_message",
+                    type: "text",
+                },
+                {
+                    name: "last_message_time",
+                    type: "timestamp with time zone"
+                }
+            ],
+            foreignKeys: [
+                {
+                    name: "UserId",
+                    columnNames: ["user_id"],
+                    referencedTableName: "user",
+                    referencedColumnNames: ["id"],
+                }
+            ],
+        }));
+    };
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("contacts");
+    };
+
+};
