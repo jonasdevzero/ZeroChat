a40b836a76e689fcbec478de233087b61de21b17
jonasdevzero <jonasdevzero@gmail.com>
2021-03-12 09:25:38 -0300

    adding addContact container

--- frontend/src/components/AddContact.tsx
+++ frontend/src/components/AddContact.tsx
@@ -1,8 +1,86 @@
+import { useState } from "react";
+import { UserI } from "../types/user";
+
+import {
+    Container,
+    Header,
+    Inner,
+} from "../styles/components/Container";
+import {
+    Form,
+    Input,
+    Contacts,
+    Contact,
+    Submit,
+    WrapperAvatar,
+    NotFound,
+    Initial,
+} from "../styles/components/AddContact";
+import { Avatar } from "@material-ui/core";
+import {
+    Search as SearchIcon,
+    Add as AddIcon,
+} from "@material-ui/icons";
 
 export default function AddContact() {
+    const [username, setUsername] = useState("");
+
+    const [contacts, setContact] = useState<UserI[]>();
+    const [notFound, setNotFound] = useState(false);
+
+    function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        //...
+    };
+
+    function add(contact: UserI) {
+        //...
+    };
+
     return (
-        <div>
-            <h1>Profile</h1>
-        </div>
+        <Container>
+            <Header>
+                <h1>Add Contact</h1>
+            </Header>
+
+            <Inner>
+                <Form onSubmit={onSubmit}>
+                    <Input
+                        placeholder="Type an username"
+                        type="text"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+
+                    <Submit type="submit">
+                        <SearchIcon />
+                    </Submit>
+                </Form>
+
+                <Contacts>
+                    {contacts ? contacts.map((contact, i) => {
+                        return (
+                            <Contact onClick={() => add(contact)}>
+                                <WrapperAvatar>
+                                    <Avatar src={contact.picture} />
+                                    <p>{contact.username}</p>
+                                </WrapperAvatar>
+
+                                <AddIcon fontSize="large" />
+                            </Contact>
+                        );
+                    }) : notFound ? (
+                        <NotFound>
+                            <strong>No users found!</strong>
+                        </NotFound>
+                    ) : (
+                        <Initial>
+                            <strong>search for users to add to your contacts</strong>
+                        </Initial>
+                    )}
+                </Contacts>
+            </Inner>
+        </Container>
     );
 };
---
+++ frontend/src/styles/components/AddContact.ts
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+    width: 100%;
+    padding: 0 2rem 2rem 2rem;
+    position: relative;
+
+    border-bottom: solid .1rem #777;
+`;
+
+export const Input = styled.input`
+    width: 100%;
+    height: 3.5rem;
+
+    padding: 0 3.3rem 0 1rem;
+    border: none;
+    outline: none;
+    border-radius: 1.5rem;
+
+    color: lightgray;
+    background-color: #353535;
+`;
+
+export const Submit = styled.button`
+    position: absolute;
+    top: .6rem;
+    right: 2.9rem;
+
+    border: none;
+    outline: none;
+    background-color: transparent;
+
+    & > .MuiSvgIcon-root {
+        font-size: 2.2rem;
+        color: lightgray;
+        cursor: pointer;
+    };
+`;
+
+export const Contacts = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 100%;
+    margin-top: 3rem;
+    padding: 0 2rem;
+`;
+
+export const Contact = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: .5rem;
+    padding: 1rem;
+    border-radius: .3rem;
+
+    background-color: #333;
+    transition: background-color .3s ease;
+
+    cursor: pointer;
+
+    p {
+        margin-left: .7rem;
+        font-size: 1.6rem;
+
+    };
+
+    &:hover {
+        background-color: #303030;
+    };
+`;
+
+export const WrapperAvatar = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const NotFound = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 100%;
+    height: 6rem;
+
+    strong {
+        color: lightgray;
+        font-size: 2rem;
+    };
+`;
+
+export const Initial = styled(NotFound)``;
