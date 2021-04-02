9d3086cced0786c4a5f710d1b9e62968631b9a22
jonasdevzero <jonasdevzero@gmail.com>
2021-04-01 22:19:37 -0300

    adding the group messages view

--- backend/src/views/index.ts
+++ backend/src/views/index.ts
@@ -2,3 +2,4 @@ export { default as UserView } from "./UserView";
 export { default as ContactView } from "./ContactView";
 export { default as GroupView } from "./GroupView";
 export { default as GroupUsersView } from "./GroupUsersView";
+export { default as GroupMessagesView } from './GroupMessagesView';
