010ba33e878b40735e18945cd741b244ee5d6a56
jonasdevzero <jonasdevzero@gmail.com>
2021-04-01 22:21:21 -0300

    adding two services: - socket - user

---
+++ frontend/src/services/index.ts
@@ -0,0 +1,3 @@
+export { default as api } from './api';
+export { default as socket } from './socket';
+export { default as userService } from './user'
---
+++ frontend/src/services/socket.ts
@@ -0,0 +1,10 @@
+import io from "socket.io-client";
+
+const URL = 'http://localhost:3001';
+
+const socket = io(URL, {
+    autoConnect: false,
+    transports: ['websocket']
+});
+
+export default socket;
