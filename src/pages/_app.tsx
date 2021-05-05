4acae130a71e212495f96010692a0cc30a0c7512
jonasdevzero <jonasdevzero@gmail.com>
2021-05-04 22:21:04 -0300

    adding react redux provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,23 @@
 import GlobalStyle from '../styles/global';
 import { ThemeProvider } from 'styled-components';
 import usePersistedState from '../hooks/usePersistedState';
-import { light, dark } from '../styles/theme';
+import * as Themes from '../styles/theme';
+import { Provider } from 'react-redux'
+import store from '../store'
 
 function MyApp({ Component, pageProps }) {
     const [theme, setTheme] = usePersistedState<"light" | "dark">("theme", "dark");
 
     return (
-        <ThemeProvider theme={theme === 'light' ? light : dark}>
+        <ThemeProvider theme={Themes[theme]}>
             <GlobalStyle />
+            <Provider store={store}>
                 <Component
                     {...pageProps}
                     theme={theme}
                     setTheme={setTheme}
                 />
+            </Provider>
         </ThemeProvider>
     );
 };
