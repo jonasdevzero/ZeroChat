c8f82e062743ac3813c56b0a3911c23ef3438f43
jonasdevzero <jonasdevzero@gmail.com>
2021-05-17 23:34:40 -0300

    refactor sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,42 @@
-import { useRouter } from "next/router";
 import { useState, useEffect, Dispatch, SetStateAction } from "react";
+import { useRouter } from "next/router";
 import Fuse from "fuse.js";
 import Cookies from 'js-cookie';
 import { useSelector, useDispatch } from 'react-redux'
-import { toggleCallMinimized } from '../store/actions/call'
-import { setCurrentRoom, removeCurrentRoom } from '../store/actions/currentRoom'
+import { usePersistedState } from '../hooks'
+import { socket } from '../services'
+import * as UserActions from '../store/actions/user'
+import * as CurrentRoomActions from '../store/actions/currentRoom'
+import * as CurrentContainerActions from '../store/actions/currentContainer'
+import * as CallActions from '../store/actions/call'
 
 import { UserI } from "../types/user";
 
 import {
     Container,
-    Header,
+    Options,
     User,
+    OptionsInner,
+    OptionsPlus,
+    Option,
     Inner,
-    RoomsContainer,
+    Header,
+    Title,
     Rooms,
     Search,
+    SearchInputWrapper,
     SearchInput,
     SearchButton,
+    ContactFilters,
+    Filter,
     Room,
-    Options,
     Status,
-    OptionButton,
     UnreadMessages
 } from "../styles/components/Sidebar";
 import { Avatar } from "@material-ui/core";
 import {
     Person as PersonIcon,
     Group as GroupIcon,
-    PersonAdd as PersonAddIcon,
-    GroupAdd as GroupAddIcon,
     Brightness3 as Brightness3Icon,
     Brightness7 as Brightness7Icon,
     PowerSettingsNew as PowerSettingsNewIcon,
@@ -38,145 +45,138 @@ import {
 } from "@material-ui/icons";
 
 interface SidebarI {
-    user: UserI;
-    currentContainer: 'profile' | "messages" | "addContact" | "createGroup";
-    setCurrentContainer: Dispatch<SetStateAction<'profile' | "messages" | "addContact" | "createGroup">>;
     theme: "light" | "dark";
     setTheme: Dispatch<SetStateAction<"light" | "dark">>;
 };
 
-export default function Sidebar({
-    user,
-    currentContainer,
-    setCurrentContainer,
-    theme,
-    setTheme,
-}: SidebarI) {
+export default function Sidebar({ theme, setTheme }: SidebarI) {
+    const user: UserI = useSelector((state: any) => state.user)
     const [rooms, setRooms] = useState<any[]>(user.contacts)
-    const [search, setSearch] = useState("")
-    const [searchResult, setSearchResult] = useState<any[]>(undefined)
 
-    const callMinimized = useSelector((state: any) => state.call.minimized)
+    const [search, setSearch] = useState("")
 
     const [roomsType, setRoomsType] = useState<"contacts" | "groups">("contacts")
+
+    const [filter, setFilter] = usePersistedState<"all" | "online" | "offline">("contact_list_filter", "all")
 
     const router = useRouter()
     const dispatch = useDispatch()
 
     useEffect(() => {
-        const fuse = new Fuse(rooms, { keys: ["name", "username"] });
-        const result = fuse.search(search).map(({ item }) => item);
-        setSearchResult(result);
-    }, [search, user.contacts, user.groups]);
+        socket.on("new-group", (group) => {
+            socket.emit("join-group", group.id)
+            dispatch(UserActions.pushRoom({ roomType: 'group', room: group }))
+        })
+        socket.on('update', action => dispatch(action))
+    }, [])
 
-    useEffect(() => roomsType === "contacts" ? setRooms(user.contacts) : setRooms(user.groups), [roomsType, user.contacts, user.groups]);
+    useEffect(() => {
+        const fuse = new Fuse(rooms, { keys: ['name', 'username'] })
+        search.length ? setRooms(fuse.search(search).map(({ item }) => item)) : setRooms(user[roomsType])
+    }, [search])
 
-    function changeContainer(option: 'profile' | "messages" | "addContact" | "createGroup", type?: "contacts" | "groups") {
-        option === "messages" ? setRoomsType(type) : dispatch(removeCurrentRoom());
-        setCurrentContainer(option);
-    };
+    function changeRooms(type: "contacts" | "groups") {
+        setRooms(user[type])
+        setRoomsType(type)
+    }
 
-    function selectRoom(room: any, type: "contact" | "group") {
-        setSearch("");
-        setCurrentContainer("messages");
-        dispatch(setCurrentRoom(room, type))
-    };
-
-    function toggleTheme() {
-        setTheme(theme === "dark" ? "light" : "dark");
-    };
+    function selectRoom(room: any) {
+        const roomType = roomsType === 'contacts' ? 'contact' : 'group'
+        setSearch('')
+        dispatch(CurrentRoomActions.setCurrentRoom(room, roomType))
+    }
 
     function logOut() {
-        Cookies.remove('token');
-        router.replace("/");
-    };
+        Cookies.remove('token')
+        router.replace('/')
+    }
 
     return (
         <Container>
-            <Header>
-                <User onClick={() => changeContainer('profile')}>
+            <Options>
+                <User>
                     <Avatar src={user.picture} />
-                    <h2>{user.username}</h2>
                 </User>
-            </Header>
+
+                <OptionsInner>
+                    <Option onClick={() => changeRooms("contacts")}>
+                        <PersonIcon />
+                    </Option>
+
+                    <Option onClick={() => changeRooms("groups")}>
+                        <GroupIcon />
+                    </Option>
+
+                    <Option onClick={() => dispatch(CallActions.toggleCallMinimized())}>
+                        <CallIcon />
+                    </Option>
+                </OptionsInner>
+
+                <OptionsPlus>
+                    <Option onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} >
+                        {theme === "dark" ? (<Brightness3Icon />) : (<Brightness7Icon />)}
+                    </Option>
+
+                    <Option onClick={() => logOut()} >
+                        <PowerSettingsNewIcon />
+                    </Option>
+                </OptionsPlus>
+            </Options>
 
             <Inner>
-                <Options>
-                    <div>
-                        <OptionButton
-                            className={`option ${currentContainer === "messages" && roomsType === "contacts" && "selected"}`}
-                            onClick={() => changeContainer("messages", "contacts")}
-                        >
-                            <PersonIcon />
-                        </OptionButton>
+                <Header>
+                    <Title>Contacts</Title>
 
-                        <OptionButton
-                            className={`option ${currentContainer === "messages" && roomsType === "groups" && "selected"}`}
-                            onClick={() => changeContainer("messages", "groups")}
-                        >
-                            <GroupIcon />
-                        </OptionButton>
+                    {rooms.find(room => room.unread_messages > 0) ? (
+                        <UnreadMessages className='total'>
+                            {rooms.reduce((acc, crr) => acc += crr.unread_messages, 0)}
+                        </UnreadMessages>
+                    ) : null}
+                </Header>
 
-                        <OptionButton
-                            className={`option ${currentContainer === "addContact" && "selected"}`}
-                            onClick={() => changeContainer("addContact")}
-                        >
-                            <PersonAddIcon />
-                        </OptionButton>
-
-                        <OptionButton
-                            className={`option ${currentContainer === "createGroup" && "selected"}`}
-                            onClick={() => changeContainer("createGroup")}
-                        >
-                            <GroupAddIcon />
-                        </OptionButton>
-                    </div>
-
-                    <div>
-                        {callMinimized ? (
-                            <OptionButton className='option' onClick={() => dispatch(toggleCallMinimized())}>
-                                <CallIcon />
-                            </OptionButton>
-                        ) : null}
-
-                        <OptionButton className="theme" onClick={() => toggleTheme()} >
-                            {theme === "dark" ? (<Brightness3Icon />) : (<Brightness7Icon />)}
-                        </OptionButton>
-
-                        <OptionButton className="off" onClick={() => logOut()} >
-                            <PowerSettingsNewIcon />
-                        </OptionButton>
-                    </div>
-                </Options>
-
-                <RoomsContainer>
-                    <Search>
+                <Search>
+                    <SearchInputWrapper>
                         <SearchInput
                             type="text"
-                            placeholder={`Find a ${roomsType}`}
+                            placeholder={`Find ${roomsType}`}
                             value={search}
                             onChange={e => setSearch(e.target.value)}
                         />
-
                         <SearchButton type="button">
                             <SearchIcon />
                         </SearchButton>
-                    </Search>
+                    </SearchInputWrapper>
 
-                    <Rooms>
-                        {(search.length > 0 ? searchResult : rooms).map(room => {
-                            return (
-                                <Room key={room.id} onClick={() => selectRoom(room, roomsType === "contacts" ? "contact" : "group")}>
-                                    <Avatar src={room.image} />
-                                    <h3>{roomsType === "contacts" ? room.username : room.name}</h3>
+                    {roomsType === 'contacts' ? (
+                        <ContactFilters>
+                            <Filter className={filter === 'all' && 'selected'} type="button" onClick={() => setFilter("all")}>
+                                All
+                            </Filter>
 
-                                    {roomsType === "contacts" ? (<Status className={room.online ? "online" : "offline"} />) : null}
-                                    {room.unread_messages > 0 ? (<UnreadMessages>{room.unread_messages}</UnreadMessages>) : null}
-                                </Room>
-                            );
-                        })}
-                    </Rooms>
-                </RoomsContainer>
+                            <Filter className={filter === 'online' && 'selected'} type="button" onClick={() => setFilter("online")}>
+                                Online
+                            </Filter>
+
+                            <Filter className={filter === 'offline' && 'selected'} type="button" onClick={() => setFilter("offline")}>
+                                Offline
+                            </Filter>
+                        </ContactFilters>
+                    ) : null}
+                </Search>
+
+                <Rooms>
+                    {rooms.map(room => {
+                        return (
+                            <Room key={room.id} onClick={() => selectRoom(room)}>
+                                <Avatar src={room.image} />
+                                <h3>{roomsType === "contacts" ? room.username : room.name}</h3>
+
+                                {roomsType === "contacts" ? (<Status className={room.online ? "online" : "offline"} />) : null}
+                                {room.unread_messages > 0 ? (<UnreadMessages>{room.unread_messages}</UnreadMessages>) : null}
+                            </Room>
+                        );
+                    })}
+                </Rooms>
             </Inner>
         </Container>
     );
